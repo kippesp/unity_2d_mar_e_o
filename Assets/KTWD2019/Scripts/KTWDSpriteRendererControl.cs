d4cbcd7e4107d60ac44d07c107bd0083aef96ed6
Paul Kippes <kippesp@gmail.com>
2019-06-15 21:09:54 -0700

    Add enable/disable to rendering of pow button

---
+++ Assets/KTWD2019/Scripts/KTWDSpriteRendererControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KTWDSpriteRendererControl : MonoBehaviour
+{
+    private SpriteRenderer srenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        srenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    public void SetEnabled()
+    {
+        srenderer.enabled = true;
+    }
+    public void SetDisabled()
+    {
+        srenderer.enabled = false;
+    }
+}
